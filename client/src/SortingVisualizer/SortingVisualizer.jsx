3eb01ae1a31b8b1cf30bf064e6bda30a1e8bdd64
DanielYanger <danielyang2002@gmail.com>
2020-07-30 16:07:10 -0500

    Variable Name updates

--- client/src/SortingVisualizer/SortingVisualizer.jsx
+++ client/src/SortingVisualizer/SortingVisualizer.jsx
@@ -4,7 +4,15 @@ import "./SortingVisualizer.css";
 import { getMergeSortAnimations } from "./mergeSort.js";
 import { getBubbleSortAnimations } from "./bubbleSort.js";
 
-const num_array_bars = 100;
+const num_array_bars = 300;
+const max_height_bar = 600;
+
+const primary_color = "turquoise";
+const secondary_color = "red";
+const finish_color = "lawngreen";
+
+const merge_sort_speed = 10;
+const bubble_sort_speed = 3;
 
 export default class SortingVisualizer extends Component {
   constructor(props) {
@@ -21,14 +29,13 @@ export default class SortingVisualizer extends Component {
   resetArray() {
     const array = [];
     for (let i = 0; i < num_array_bars; i++) {
-      array.push(randomIntFromInterval(10, 500));
+      array.push(randomIntFromInterval(10, max_height_bar));
     }
     this.setState({ array });
   }
 
   bubbleSort() {
     const animations = getBubbleSortAnimations(this.state.array);
-    console.log(animations);
     const arrayBars = document.getElementsByClassName("array-bar");
     for (let i = 0; i < animations.length; i++) {
       const isColorChange = i % 3 !== 2;
@@ -36,24 +43,24 @@ export default class SortingVisualizer extends Component {
         const [barOneIdx, barTwoIdx] = animations[i];
         const barOneStyle = arrayBars[barOneIdx].style;
         const barTwoStyle = arrayBars[barTwoIdx].style;
-        const color = i % 3 === 0 ? "red" : "turquoise";
+        const color = i % 3 === 0 ? secondary_color : primary_color;
         setTimeout(() => {
           barOneStyle.backgroundColor = color;
           barTwoStyle.backgroundColor = color;
-        }, i * 3);
+        }, i * bubble_sort_speed);
       } else {
         setTimeout(() => {
           const [barOneIdx, newHeight] = animations[i];
           const barOneStyle = arrayBars[barOneIdx].style;
           const height = newHeight + "px";
           barOneStyle.height = height;
-        }, i * 3);
+        }, i * bubble_sort_speed);
       }
     }
     for (let i = 0; i < arrayBars.length; i++) {
       setTimeout(() => {
-        arrayBars[i].style.backgroundColor = "LawnGreen";
-      }, (animations.length + i) * 3);
+        arrayBars[i].style.backgroundColor = finish_color;
+      }, (animations.length + i) * bubble_sort_speed);
     }
   }
 
@@ -66,24 +73,24 @@ export default class SortingVisualizer extends Component {
         const [barOneIdx, barTwoIdx] = animations[i];
         const barOneStyle = arrayBars[barOneIdx].style;
         const barTwoStyle = arrayBars[barTwoIdx].style;
-        const color = i % 3 === 0 ? "red" : "turquoise";
+        const color = i % 3 === 0 ? secondary_color : primary_color;
         setTimeout(() => {
           barOneStyle.backgroundColor = color;
           barTwoStyle.backgroundColor = color;
-        }, i * 10);
+        }, i * merge_sort_speed);
       } else {
         setTimeout(() => {
           const [barOneIdx, newHeight] = animations[i];
           const barOneStyle = arrayBars[barOneIdx].style;
           const height = newHeight + "px";
           barOneStyle.height = height;
-        }, i * 10);
+        }, i * merge_sort_speed);
       }
     }
     for (let i = 0; i < arrayBars.length; i++) {
       setTimeout(() => {
-        arrayBars[i].style.backgroundColor = "LawnGreen";
-      }, (animations.length + i) * 10);
+        arrayBars[i].style.backgroundColor = finish_color;
+      }, (animations.length + i) * merge_sort_speed);
     }
   }
 
@@ -96,7 +103,7 @@ export default class SortingVisualizer extends Component {
             className="array-bar"
             key={idx}
             style={{
-              backgroundColor: "turquoise",
+              backgroundColor: primary_color,
               height: `${value}px`,
             }}
           ></div>
@@ -106,7 +113,7 @@ export default class SortingVisualizer extends Component {
           key="max-height-controller"
           style={{
             backgroundColor: "transparent",
-            height: `500px`,
+            height: `${max_height_bar}px`,
           }}
         ></div>
         <br></br>
